a4a9b27ce200d8a88bce22c9d129ab828675f44d
Emmanuel Omole <emmanueliyare7@gmail.com>
2023-10-27 15:23:04 +0100

    user resource implementation

--- index.js
+++ index.js
@@ -95,6 +95,41 @@ const deleteBank =  (req, res) => {
     })
 }
 
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined"
+    })
+}
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined"
+    })
+}
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined"
+    })
+}
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined"
+    })
+}
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined"
+    })
+}
+
 // app.get('/api/v1/banks', getAllBanks);
 // app.get('/api/v1/banks/:id', getBank);
 // app.post("/api/v1/banks", createBank);
